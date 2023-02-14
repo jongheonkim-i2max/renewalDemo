94aa4b006aa95d14f0158197b7f1b6b185ea2518
jongheonK <jhk@i2max.co.kr>
2023-02-14 10:28:57 +0900

    demoSite setting

---
+++ js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(){
+    /*aos start*/
+    AOS.init();
+});
+
+/*main top banner*/
+/*
+let mainBannerSwiper = new Swiper('.main-banner .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween:60,
+    loop: true,
+    observer: true, 
+    observeParents: true,
+    //loopedSlides: 1,
+    speed:1000,
+    autoplay:false,
+    autoplay: {
+        delay : 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        360: {
+            slidesPerView: 1,
+            spaceBetween:15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween:40,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween:40
+        },
+        1200:{
+            spaceBetween:40
+        },
+        1201:{
+            spaceBetween:60
+        }
+    },
+});
+*/
